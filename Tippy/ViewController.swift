a9726d6cb8558cdf45c9c480e2913feb9a75792a
steveroy126 <56884185+steveroy126@users.noreply.github.com>
2021-01-08 14:40:21 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Steve Roy on 1/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
